f6f48c8576e68d13dd7ff6056221bd5f28e153da
Josh9638 <joshua.sailemab@ug.edu.ec>
2023-11-15 22:20:50 -0500

    ms-core-sales-system version 1.0.0

--- src/main/java/io/jscode/util/Constantes.java
+++ src/main/java/io/jscode/util/Constantes.java
@@ -4,6 +4,11 @@ public class Constantes {
 	public static final Integer ERROR_CODE = -1;
 	public static final String ERROR_STATUS = "ERROR";
 	public static final String ESTADO_INACTIVO = "Inactivo";
+	public static final String ESTADO_ELIMINADO = "Eliminado";
 	public static final String ESTADO_ACTIVO = "Activo";
+	public static final String ESTADO_ANULADO = "Anulado";
 	public static final String IP_DEFAULT = "127.0.0.1";
+	public static final int OPCION_6_MESES = 1;
+	public static final int OPCION_30_DIAS = 2;
+	public static final int OPCION_7_DIAS = 3;
 }
--- src/main/java/io/jscode/util/SalesUtils.java
+++ src/main/java/io/jscode/util/SalesUtils.java
@@ -1,13 +1,13 @@
 package io.jscode.util;
 
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,7 +27,7 @@ public class SalesUtils {
 		T response;
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.registerModule(new JavaTimeModule());
-		mapper.setDateFormat(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss"));
+		//mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));		
 		String jsonStr;
 
 		try {
@@ -45,11 +45,14 @@ public class SalesUtils {
 		List<T> response;
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.registerModule(new JavaTimeModule());
+		//mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));	
+		CollectionType listType = mapper
+		        .getTypeFactory()
+				.constructCollectionType(ArrayList.class, clase);
 		try {
 			String jsonStr = mapper.writeValueAsString(request);
-			response = mapper.readValue(jsonStr, new TypeReference<List<T>>() {
-			});
-		} catch (JsonProcessingException e) {
+			response = mapper.readValue(jsonStr, listType);
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ExcepcionGenerica(e);
 		}
